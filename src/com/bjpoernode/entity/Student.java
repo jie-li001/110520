9772ad421da34212971ee916b36eb48b155afa4b
li <li@110520>
2022-04-22 11:26:24 +0800

    版本5

--- src/com/bjpoernode/entity/Student.java
+++ src/com/bjpoernode/entity/Student.java
@@ -11,27 +11,4 @@ public class Student {
         this.age = age;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
 }
