322b558d24b9d4e1843a6eb3c46a24cc6e43c842
li <li@110520>
2022-04-22 11:20:51 +0800

    版本3

--- src/com/bjpoernode/entity/Student.java
+++ src/com/bjpoernode/entity/Student.java
@@ -4,7 +4,7 @@ public class Student {
     private int id;
     private String name;
     private String age;
-
+    private int num3;
     public Student(int id, String name, String age) {
         this.id = id;
         this.name = name;
