9de404f26997ea388f6bdbc781e818c3edee785d
li <li@110520>
2022-04-22 10:06:40 +0800

    版本1

---
+++ src/com/bjpoernode/entity/Student.java
@@ -0,0 +1,37 @@
+package com.bjpoernode.entity;
+
+public class Student {
+    private int id;
+    private String name;
+    private String age;
+
+    public Student(int id, String name, String age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
